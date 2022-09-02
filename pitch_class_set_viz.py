6d18907dfa04f693c7af0b554d6b2ee3855dbfd5
Psi <simonstolarczyk@gmail.com>
2022-09-02 09:07:10 -0500

    clean up
    Remove extraneous code and numpy dependency.

--- pitch_class_set_viz.py
+++ pitch_class_set_viz.py
@@ -1,42 +1,49 @@
-import numpy as np
+import math
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from matplotlib.patches import Circle
+from itertools import combinations
 
-def create_pitch_class_viz(pitches, filename=None):
+def create_clock_diagram(pitches, filename=None, draw_all_arcs=False, save_diagram=False):
     n_pitches = 12
-    top = np.pi / 2
-    full_rotation = 2 * np.pi
-    theta = np.linspace(top, top - full_rotation, endpoint=True, num=n_pitches+1)
-    r = 2.5
+    top = math.pi / 2
+    full_rotation = 2 * math.pi
+    angles = [top - full_rotation * (i / n_pitches) for i in range(n_pitches)]
+    r = 1
 
-    xs = r * np.cos(theta)
-    ys = r * np.sin(theta)
-
-    poly = Polygon(np.column_stack([xs, ys]), animated=True)
-
+    xs = [r * math.cos(angle) for angle in angles]
+    ys = [r * math.sin(angle) for angle in angles]
+    
+    fig, ax = plt.subplots(figsize=(10,10))
 
     circle = Circle((0, 0), r)
+    ax.add_patch(circle)
 
-    fig, ax = plt.subplots(figsize=(10,10))
-    ax.add_patch(circle,)
 
-    x, y = zip(*poly.xy)
-
-    # print(xs, ys)
     line_x = [xs[i] for i in pitches]
     line_x.append(xs[pitches[0]])
     line_y = [ys[i] for i in pitches]
     line_y.append(ys[pitches[0]])
-    # print("line")
-    # print(line_x, line_y)
+
+
+    arcs = []
+    points = list(zip(xs, ys))
+    if draw_all_arcs:
+        for p1, p2 in combinations(points, 2):
+            arc_x = [p1[0], p2[0]]
+            arc_y = [p1[1], p2[1]]
+            arc = Line2D(line_x, line_y, color="black", marker='o')
+            arcs.append(arcs)
+        for arc in arcs:
+            ax.add_line(arc)
+
     line = Line2D(line_x, line_y, color="black",
-                        marker='o', markerfacecolor='r',
-                        animated=True)
+                        marker='o', markerfacecolor='r')
     ax.add_line(line)
+
     ax.scatter(xs, ys, color="black", marker="o", zorder=2)
 
-    for i, pair in enumerate(zip(x[:-1],ys[:-1])):
+    for i, pair in enumerate(zip(xs,ys)):
         coords = [1.1 * p for p in pair]
         ax.annotate(i, xy=coords)
 
@@ -44,7 +51,12 @@ def create_pitch_class_viz(pitches, filename=None):
     ax.set_xlim((-2, 2))
     ax.set_ylim((-2, 2))
     ax.set_title(f"{pitches}")
-    ax.set_facecolor("grey")
-    # plt.show()
     if filename is not None:
         fig.savefig(filename)
+    if save_diagram:
+        pitch_string = "-".join([str(p) for p in pitches])
+        fig.savefig(f"{pitch_string}.png")
+
+if __name__ == "__main__":
+    create_clock_diagram([0, 1, 4], save_diagram=True)
+    create_clock_diagram([0, 2, 7], "test2.png")
